61a1f113d22e173fd42f77b74946ad455251737f
jiang1590 <jiangxingfei1590@163.com>
2014-01-22 14:33:33 +0800

    Initial Commit

---
+++ 动画/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  动画
+//
+//  Created by 蒋 杏飞 on 14-1-22.
+//  Copyright (c) 2014年 蒋 杏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 动画/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  动画
+//
+//  Created by 蒋 杏飞 on 14-1-22.
+//  Copyright (c) 2014年 蒋 杏飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
